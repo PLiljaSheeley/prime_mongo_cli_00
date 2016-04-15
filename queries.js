ac6c1dddebbab3ed752ff3cd439de1d8bae4ea45
Peter Lilja-Sheeley <Peter@Peters-MacBook-Pro.local>
2016-04-15 10:09:14 -0500

    first commit

---
+++ queries.js
@@ -0,0 +1,7 @@
+db.bios.find({awards: {$exists:true}}).pretty()
+db.bios.find({awards: {$exists:false}}).pretty()
+db.bios.find({$or: [{"contribs": "OOP"}, {"contribs": "UNIX"}]}).pretty()
+db.bios.find({"awards":{$elemMatch: {"award":"Turing Award"}}}).pretty()
+db.bios.find({$and:[{"_id": {$lt: 7, $gt: 3}}]}).pretty()
+db.bios.find({"awards": {$elemMatch: {"year": {$lt: 2000}}}}).pretty()
+db.bios.find({ death: {$exists: false}}).pretty()
